20e87f532acde266659ab563b39bbf738dcc9917
Robin Opletal <me@robinopletal.com>
2023-07-17 08:33:30 +0200

    playing with layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="bg-amber-50 flex min-h-screen flex-col items-center justify-between p-12">
-    <div className="bg-gradient-to-tr hover:bg-gradient-to-bl p-[15px] from-[#7928ca] to-[#ff0080]">
+    <main className="flex min-h-screen flex-col items-center pb-8 justify-between">
+    <div className="bg-gradient-to-tr hover:bg-gradient-to-bl p-[30px] from-[#7928ca] to-[#ff0080] w-screen text-center">
         <h1 className='text-8xl text-stone-200 font-sans'>Testing</h1>
     </div>
-    <div className='grid grid-flow-row md:grid-flow-col items-center justify-items-center w-full gap-y-16'>
+    <div className='grid grid-flow-row md:grid-flow-col items-center justify-items-center w-full gap-y-8'>
         <Image
             src='/next.svg'
             alt='next logo'
@@ -16,6 +16,12 @@ export default function Home() {
         <Image
             src='/vercel.svg'
             alt='vercel logo'
+            width='200'
+            height='30'
+        />
+        <Image
+            src='/next.svg'
+            alt='next logo'
             width='200'
             height='30'
         />
