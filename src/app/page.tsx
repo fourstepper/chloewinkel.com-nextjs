a168b9f4e1e1bfd7a172239b76346d237f6dceeb
Robin Opletal <me@robinopletal.com>
2023-07-22 11:34:05 +0200

    feat(motion): start playing with framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,52 @@
 import * as c from "../components/Components";
 import Image from "next/image";
 import Link from "next/link";
+import Motion from "../components/Motion";
 
 export default function Home() {
   return (
     <main>
       <c.h1>Chloe Winkel</c.h1>
+      <c.FlexGrid>
+        <div>
+          <c.p>Brands</c.p>
+          <Link href="branding">
+            <div className="grow relative w-[300px] sm:w-[600px] md:w-[750px] lg:w-[900px] h-36">
+              <Image src="/banner.svg" alt="branding" fill={true} />
+            </div>
+          </Link>
+        </div>
+      </c.FlexGrid>
+      <c.FlexGrid>
+        <div>
+          <c.p>Brands</c.p>
+          <Link href="branding">
+            <div className="grow relative w-[300px] sm:w-[600px] md:w-[750px] lg:w-[900px] h-36">
+              <Image src="/banner.svg" alt="branding" fill={true} />
+            </div>
+          </Link>
+        </div>
+      </c.FlexGrid>
+      <c.FlexGrid>
+        <div>
+          <c.p>Brands</c.p>
+          <Link href="branding">
+            <div className="grow relative w-[300px] sm:w-[600px] md:w-[750px] lg:w-[900px] h-36">
+              <Image src="/banner.svg" alt="branding" fill={true} />
+            </div>
+          </Link>
+        </div>
+      </c.FlexGrid>
+      <c.FlexGrid>
+        <div>
+          <c.p>Brands</c.p>
+          <Link href="branding">
+            <div className="grow relative w-[300px] sm:w-[600px] md:w-[750px] lg:w-[900px] h-36">
+              <Image src="/banner.svg" alt="branding" fill={true} />
+            </div>
+          </Link>
+        </div>
+      </c.FlexGrid>
       <c.FlexGrid>
         <div>
           <c.p>Brands</c.p>
@@ -24,6 +65,9 @@ export default function Home() {
               <Image src="/banner.svg" alt="branding" fill={true} />
             </div>
           </Link>
+          <div className="justify-items-center items-center text-center">
+            <Motion></Motion>
+          </div>
         </div>
       </c.FlexGrid>
     </main>
