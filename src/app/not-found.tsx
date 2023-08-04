192b1ae5554237bf829378bb10172ec32f2ea30b
Robin Opletal <me@robinopletal.com>
2023-08-04 23:27:08 +0200

    refactor(components): change BackHome up

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,11 @@
-import Link from "next/link";
+import { BackHome } from "@BackHome";
 
 const NotFoundError = () => {
   return (
     <div className="fixed top-[30%]">
       <h1 className="text-8xl">Page not found, sorry!</h1>
-      <div className="py-16"></div>
-      <Link href="/">
-        <p className="text-4xl text-center underline decoration-indigo-500 decoration-wavy underline-offset-8 decoration-2">
-          Back Home
-        </p>
-      </Link>
+      <div className="py-16" />
+      <BackHome />
     </div>
   );
 };
--- src/components/BackHome.tsx
+++ src/components/BackHome.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export const BackHome = () => {
   return (
     <Link href="/">
-      <p className="text-center text-lg">Back Home</p>
+      <p className="text-center text-4xl underline decoration-indigo-500 decoration-wavy decoration-2 underline-offset-8">
+        Back Home
+      </p>
     </Link>
   );
 };
