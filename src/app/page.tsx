68d468ff569c1d75ce50605ea891b33f5bec97f2
Robin Opletal <me@robinopletal.com>
2023-08-05 20:31:54 +0200

    refactor(homepage): slight adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Link from "next/link";
 const Home = () => {
   return (
     <main>
-      <div className="container flex w-[250px] flex-col gap-8 sm:w-[450px] md:w-[600px] ">
+      <div className="container flex w-[250px] flex-col gap-8 sm:w-[450px] md:w-[600px] lg:w-[900px]">
         <Header heading="Chloe Winkel" />
         <div className="gap-y-8" />
         <Link href="graphic-design">
           <p className="grow text-center text-4xl">Graphic Design</p>
         </Link>
+        <div className="gap-y-8" />
         <Link href="/about-me">
           <Hands />
         </Link>
