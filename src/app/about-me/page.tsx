cca79d4966ba4edb6146e10e4b0cd56534480853
Robin Opletal <me@robinopletal.com>
2023-08-05 21:24:24 +0200

    feat(site): add about-me page

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@Header";
+
+const AboutMe = () => {
+  return (
+    <main>
+      <div>
+        <Header heading="About me" />
+      </div>
+    </main>
+  );
+};
+
+export default AboutMe;
